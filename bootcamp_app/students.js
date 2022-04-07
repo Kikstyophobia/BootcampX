3aa61d93cbe3aa49ea5bfc6cf6ba082f94d66ea7
Calvin Chung <calvin-chung@live.com>
2022-04-06 21:10:28 -0700

    add new queries that utilize process.argv

---
+++ bootcamp_app/students.js
@@ -0,0 +1,40 @@
+const { Pool } = require('pg');
+let argv = process.argv.slice(2);
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+pool.query('SELECT $1::text as message', ['Successfully connected to server'], (err, res) => {
+  console.log(err ? err.stack : res.rows[0].message) 
+  pool.end()
+})
+
+
+pool.query(`
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+FROM students
+JOIN cohorts ON cohorts.id = cohort_id
+LIMIT 5;
+`)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+  })
+});
+
+pool.query(`
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+FROM students
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE '%${process.argv[2]}%'
+LIMIT ${process.argv[3] || 5};
+`)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+  })
+}).catch(err => console.error('query error', err.stack));
